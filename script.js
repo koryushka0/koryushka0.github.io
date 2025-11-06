8a46cbf9c14e013e9287539b9eea9b14050765cb
koryushka0 <112032701+koryushka0@users.noreply.github.com>
2025-11-06 15:33:56 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -683,25 +683,3 @@ document.addEventListener('DOMContentLoaded', () => {
     updateCounters();
 });
 
-window.addEventListener('beforeinstallprompt', (e) => {
-    e.preventDefault();
-    let deferredPrompt = e;
-    const installBtn = document.getElementById('install-app-btn');
-    
-    if (installBtn) {
-        installBtn.style.display = 'block';
-
-        installBtn.addEventListener('click', () => {
-            installBtn.style.display = 'none';
-            deferredPrompt.prompt();
-            deferredPrompt.userChoice.then((choiceResult) => {
-                if (choiceResult.outcome === 'accepted') {
-                    console.log('User accepted the install prompt');
-                } else {
-                    console.log('User dismissed the install prompt');
-                }
-                deferredPrompt = null;
-            });
-        });
-    }
-});
